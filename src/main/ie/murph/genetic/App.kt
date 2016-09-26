efa51d65afe580755b3ce7683bc3edf9b5ed2599
Mur3ph <paulmurphymainstreet@gmail.com>
2016-09-26 13:54:16 +0100

    Generate Random() intergers to populate a List

--- src/main/ie/murph/genetic/App.kt
+++ src/main/ie/murph/genetic/App.kt
@@ -1,5 +1,7 @@
 package main.ie.murph.genetic
 
+import java.util.*
+
 /**
  * Created by Paul on 9/25/2016.
  */
@@ -8,4 +10,12 @@ package main.ie.murph.genetic
 fun main(args: Array<String>)
 {
     println("Creating a genetic algorithm using Kotlin")
+    generateFiveRandomPopulationNumbers()
+}
+
+fun generateFiveRandomPopulationNumbers()
+{
+    //Create a list of interger type and populate  //Generate a random number between 1 and 10
+    val listOfRandomNumbers = IntArray(5){Random().nextInt(9)+1}.asList()
+    listOfRandomNumbers.forEach { println(it) }
 }
