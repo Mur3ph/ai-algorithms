5d3958f67bca4e80451d24895a8377115dd2dfec
Mur3ph <paulmurphymainstreet@gmail.com>
2016-09-30 22:45:22 +0100

    Populating a hash map from items in a list

--- src/main/ie/murph/genetic/configuration/App.kt
+++ src/main/ie/murph/genetic/configuration/App.kt
@@ -12,9 +12,22 @@ fun main(args: Array<String>)
 {
     println("Creating a genetic algorithm using Kotlin")
     val population = Population()
-    Population().generateFiveRandomPopulationNumbers().forEach { println(it)}
+    population.generateFiveRandomPopulationNumbers().forEach { println(it)}
+//    Population().generateFiveRandomPopulationNumbers().forEach { println(it)}
 
     //Next trying to place fittness into a map
-    val map = Population().generateFiveRandomPopulationNumbers()
+    val map = HashMap<String, Int>()
+    var index = 1
+    for (fitness in population.generateFiveRandomPopulationNumbers())
+    {
+        map.put("Population:"+index, fitness)
+        index++
+    }
+
+    for ((string, fittnes) in map.entries)
+    {
+        println("User $string has $fittnes fittness")
+    }
+
 }
 
